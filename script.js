05be6c17e43b87ca5a69d3ca8ba243e305445669
proking3089yt30 <150360282+proking3089yt30@users.noreply.github.com>
2023-11-09 11:05:00 -0500

    update

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const form = document.querySelector('form');
+const radios = form.elements['radio'];
 
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            window.open(`http://www.${radios[i].value}.com`);
+            break;
+        }
+    }
+});
